bf4be4eff856ac9aafbf6bbe72482ea5494d1f12
Skip <skip@usecurling.com>
2026-08-24 17:57:13 -0300

    Auto-commit after full sync (build simulation)

--- pocketbase/hooks/on_subscriber_payment_change.js
+++ pocketbase/hooks/on_subscriber_payment_change.js
@@ -23,7 +23,8 @@ onRecordAfterUpdateSuccess((e) => {
   const authToken = $secrets.get('EXTERNAL_SYSTEM_AUTH_TOKEN') || ''
   const cfClientId = $secrets.get('CF_ACCESS_CLIENT_ID') || ''
   const cfClientSecret = $secrets.get('CF_ACCESS_CLIENT_SECRET') || ''
-  console.log('Entrou no mudar Pagamento!')
+  const url = $secrets.get('EXTERNAL_SYSTEM_API_URL') || ''
+  console.log('Entrou no mudar Pagamento! ', url)
   try {
     var res = $http.send({
       url: 'https://api.vlsolucoesia.com.br/backend/v1/users',
